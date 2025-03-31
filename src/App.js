e676d23ca62b1015491ed62431b6dd5dca5dd7c1
kissagnesa <sindzse88@gmail.com>
2025-03-31 16:44:51 +0200

    ChessCreate created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, NavLink, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { ChessList } from './ChessList';
+import { ChessCreate} from './ChessCreate';
+import { ChessDel} from './ChessDel';
+import { ChessMod } from './ChessMod';
 
-function App() {
+
+export const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">Sakkozók</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/chess-create">Új sakkozó</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ChessList />} />    
+        <Route path="/chess-mod/:chessId" element={<ChessMod />} />
+        <Route path="/chess-create" element={<ChessCreate />} />
+        <Route path="/chess-del/:chessId" element={<ChessDel />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
